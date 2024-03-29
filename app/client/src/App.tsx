c5026e8170893b1362e9d6837979840c53ca5242
andrewluetgers <andrew.luetgers@gmail.com>
2024-03-29 05:24:43 -0500

    cleanup

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -12,8 +12,11 @@ export default function App() {
 			<button>Spin Up A Project</button>
 
 			<div className="mx-auto my-16 p-8 bg-black bg-opacity-75 rounded-lg">
-				<div>
-					<a href="https://vitejs.dev" target="_blank">
+				<div className="logo-container">
+					<a href="https://turbo.build/repo" target="_blank" >
+						<img src="/turborepo.svg" className="logo turbo" />
+					</a>
+					<a href="https://vitejs.dev" target="_blank" >
 						<img src="/vite.svg" className="logo vite" />
 					</a>
 					<a href="https://react.dev" target="_blank">
@@ -22,9 +25,9 @@ export default function App() {
 				</div>
 
 				<h1>
-					<span className="vite">Vite</span> +
-					<span className="react"> React</span> +
-					<span> cssed</span>
+					<span className="turbo">TURBOREPO</span> +
+					<span className="vite"> Vite</span> +
+					<span className="react"> React</span>
 				</h1>
 
 
@@ -46,12 +49,18 @@ export default function App() {
 
 let {styles} = css`
 	:local(.styles) {
-
 		color: white;
 		background-image: url('/images/spinupOrchestra.png');
 
 		h1 {
 			font-weight: bold;
+			font-size: 3em;
+		}
+
+		h1 .turbo {
+			color: #101010;
+			font-size: 0.6em;
+			text-shadow: 0 0 30px rgba(255, 255, 255, 1);
 		}
 
 		.vite {
@@ -71,26 +80,60 @@ let {styles} = css`
 		}
 
 		.logo-container {
-			display: flex; /* Enable flexbox */
-			//flex-direction: column; /* Stack logos and titles vertically */
-			align-items: center; /* Center items horizontally */
-			flex-direction: row; /* Arrange items horizontally */
-			justify-content: space-between; /* Distribute space between items */
+			display: flex;
+			align-items: center;
+			flex-direction: row;
+			justify-content: center;
 		}
 
 		.logo {
-			width: 30px; /* Adjust the width as needed */
-			margin-bottom: 5px; /* Add space between logo and title */
+			height: 150px;
+			width: auto;
+			margin-bottom: 5px;
+			padding: 1.5em 2em;
+			will-change: filter;
+			transition: filter 300ms;
 		}
 
-		/* Media query for smaller screens */
-		@media (max-width: 768px) {
-			.logo-container {
-				flex-direction: row; /* Arrange logos and titles horizontally */
-				justify-content: space-around; /* Distribute space evenly */
+		.logo:hover {
+			filter: drop-shadow(0 0 2em #646cffaa);
+		}
+
+		.logo.turbo {
+			padding-left: 100px;
+			filter: drop-shadow(0 0 50px rgba(255, 255, 255, 1));
+		}
+
+		.logo.turbo:hover {
+			filter: drop-shadow(0 0 30px rgba(255, 255, 255, 1));
+		}
+
+		.logo.react:hover {
+			filter: drop-shadow(0 0 2em #61dafbaa);
+		}
+
+		@keyframes logo-spin {
+			from {
+				transform: rotate(0deg);
+			}
+			to {
+				transform: rotate(360deg);
 			}
 		}
 
+		@media (prefers-reduced-motion: no-preference) {
+			a:nth-of-type(3) .logo {
+				animation: logo-spin infinite 20s linear;
+			}
+		}
+
+		.card {
+			padding: 2em;
+		}
+
+		.read-the-docs {
+			color: #888;
+		}
 	}
 `
 
