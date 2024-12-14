058b45af333cc5136b4c596ec49c56164e0f3900
andrewluetgers <andrew.luetgers@gmail.com>
2024-12-14 09:55:55 -0600

    Upgrade to Vite 6, Storybook 8

--- app/client/.storybook/main.js
+++ app/client/.storybook/main.js
@@ -1,19 +1,29 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-styling",
-        "@storybook/addon-mdx-gfm"
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
+    addons: [
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@storybook/addon-interactions"),
+        getAbsolutePath("@storybook/addon-styling"),
+        getAbsolutePath("@storybook/addon-mdx-gfm")
     ],
-	framework: {
-		name: "@storybook/react-vite",
+
+    framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
 		options: {},
 	},
-	docs: {
-		autodocs: "tag",
-	},
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 };
 export default config;
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
